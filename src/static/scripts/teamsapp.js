73ec40cf68fac3e3e0c07ef20ca1fe6ccee8927a
Saurabh Bhatia <sbtron@@users.noreply.github.com>
2024-03-27 14:57:56 -0700

    textbox for appid

--- src/static/scripts/teamsapp.js
+++ src/static/scripts/teamsapp.js
@@ -14,15 +14,19 @@
 
     var encodedWebUrl = encodeURIComponent('https://tasklist.example.com/123/456&label=Task 456');
       var encodedContext = encodeURIComponent(JSON.stringify({"subEntityId": "task456"}));
-      // deployed appid
-      var appid = "ee74ce78-43a4-4096-8f5f-978533ddf46b"
+      var appid = document.getElementById('appid').value;
+      if (!appid) {
+       console.log('No App Id. Refresh Page')
+       document.getElementById('deeplink').innerHTML = 'No App Id. Refresh Page';
+      } 
+      else {
       var taskItemUrl = 'https://teams.microsoft.com/l/entity/' + appid + 'index0?webUrl=' + encodedWebUrl + '&context=' + encodedContext;
       var a= document.createElement('a');
       a.href = taskItemUrl;
       a.target = '_blank';;
       a.textContent = taskItemUrl;
       document.getElementById('deeplink').appendChild(a);
-
+      }
   });
 
   function updatev2(hubName, pageid, subpageid) {
@@ -40,4 +44,18 @@
     console.log("v1subentityid: " + subEntityId); 
     document.getElementById("v1subentityid").innerHTML = "is " + subEntityId;
     }
+
+    function deeplink() {
+      var encodedWebUrl = encodeURIComponent('https://tasklist.example.com/123/456&label=Task 456');
+      var encodedContext = encodeURIComponent(JSON.stringify({"subEntityId": "task456"}));
+      var appid = document.getElementById('appid').value;
+      if (!appid) {
+        alert('Please provide an appid');
+        return;
+      }
+      var taskItemUrl = 'https://teams.microsoft.com/l/entity/' + appid + '/index0?webUrl=' + encodedWebUrl + '&context=' + encodedContext;
+      microsoftTeams.executeDeepLink(taskItemUrl);
+    } 
+    module.exports = deeplink;
+
 })();
